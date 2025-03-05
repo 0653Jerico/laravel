07990ab30b593b24694480986fc8979bb0d5727e
Jerico Jones B. Edic <144971855+0653Jerico@users.noreply.github.com>
2025-03-05 15:36:13 +0000

    Installed Tailwindcss

--- CIT18-Edic/resources/views/greet.blade.php
+++ CIT18-Edic/resources/views/greet.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/greet.csss">
+    @vite('resources/css/app.css')
     <title>Greet</title>
 </head>
 <body>
-    <h2>Hello, Welcome to Mastery in Web Technologies</h2>
+    <h2 class="text-3xl text-center font-bold mb-6">Hello, Welcome to Mastery in Web Technologies</h2>
     <p>This is a paragraph</p>
 </body>
 </html>
